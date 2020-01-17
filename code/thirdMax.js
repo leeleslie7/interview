4057a53188fca9549cd994ff22a9e71a87a9b793
liguiqi <leeleslie7@hotmail.com>
2020-01-17 15:24:22 +0800

    Update thirdMax.js
    去掉打印信息

--- code/thirdMax.js
+++ code/thirdMax.js
@@ -6,7 +6,6 @@ var thirdMax = function(nums) {
     let arr = [... new Set(nums)].sort((a,b)=>{
         return b - a
     })
-    console.log(arr)
     if(arr.length >=3){
         return arr[2]
     }else{
